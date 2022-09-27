0e1e8fbd0fb12dba96a4a43ef373b6a9cf0c4605
Aminu Babayo Shehu <absheikhone@gmail.com>
2022-09-27 15:30:40 +0100

    changed port

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.use(errorHandler);
 
 // start server
 const port =
-  process.env.NODE_ENV === "development" ? process.env.PORT || 5000 : 8080;
+  process.env.NODE_ENV === "development" ? process.env.PORT || 5000 : 3000;
 app.listen(port, () => console.log("Server listening on port " + port));
